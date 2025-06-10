2f7dfe8d5f469f40f230f258247e0c8f58a58619
NoloSibeko <sibekonolo@gmail.com>
2025-06-10 12:17:07 +0000

    Add 8-print_base16.c to print hexadecimal digits

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all hexadecimal digits in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    char c;
+
+    for (c = "0"; c <= "9"; c++)
+        putchar(c);
+    for (c = "a"; c <= "f"; c++)
+        putchar(c);
+    putchar("\n");
+
+    return (0);
+}
