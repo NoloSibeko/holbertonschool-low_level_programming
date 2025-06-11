f0d14d067578f38c097f58bb912bae8edb1d81e9
NoloSibeko <sibekonolo@gmail.com>
2025-06-11 08:07:37 +0000

    Add 0-putchar program with Betty and function restrictions

--- 0-putchar.c
+++ 0-putchar.c
@@ -3,19 +3,19 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char str[] = "_putchar";
-    int i = 0;
+	char str[] = "_putchar";
+	int i = 0;
 
-    while (str[i])
-    {
-        _putchar(str[i]);
-        i++;
-    }
-
-    _putchar('\n');
-    return (0);
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
+
--- _putchar.c
+++ _putchar.c
@@ -4,9 +4,10 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1, or -1 on error
+ * Return: On success 1, on error -1 is returned
  */
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
+
