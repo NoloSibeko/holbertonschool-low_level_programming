7411a2fa1cb54b131aa4f90d08af29881505ac46
NoloSibeko <sibekonolo@gmail.com>
2025-06-11 09:54:53 +0000

    Fix _islower to pass all test cases and Betty checks

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
 /**
- * _islower - checks if a character is lowercase
- * @c: the character to check
+ * _islower - Checks for lowercase character
+ * @c: The character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-    if (c >= 'a' && c <= 'z')
-        return (1);
-    else
-        return (0);
+    return (c >= 'a' && c <= 'z');
 }
