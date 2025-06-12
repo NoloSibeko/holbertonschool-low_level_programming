69d3a93917fa20902ccc4c33cd0e75f97ae643c2
NoloSibeko <sibekonolo@gmail.com>
2025-06-12 11:56:23 +0000

    Add reset_to_98

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include "main.h"
 
-void reset_to_98(int *n);
-
+/**
+ * main - Entry point to test reset_to_98
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    int x = 42;
-    printf("Before: %d\n", x);
-    reset_to_98(&x);
-    printf("After: %d\n", x);
-    return 0;
+	int x = 42;
+
+	printf("Before: %d\n", x);
+	reset_to_98(&x);
+	printf("After: %d\n", x);
+	return (0);
 }
