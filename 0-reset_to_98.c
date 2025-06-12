952f4e7b99c78ad2bda3c25c06e2d99d1558d0e6
NoloSibeko <sibekonolo@gmail.com>
2025-06-12 12:06:06 +0000

    Add 0-reset_to_98.c - function to reset pointer to 98

--- 0-reset_to_98.c
+++ 0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Takes a pointer to an int and updates
- *               the value it points to to 98
- * @n: Pointer to an int
+ * reset_to_98 - Updates the value pointed to by a pointer to 98
+ * @n: Pointer to an integer
  */
 void reset_to_98(int *n)
 {
